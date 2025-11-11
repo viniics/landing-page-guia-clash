f26c83d205d5cfccf92b1452eb0bea604e1da46e
Vinicius <viniipvp@gmail.com>
2025-11-11 16:32:39 -0300

    commitzin

--- lp-guia/src/App.tsx
+++ lp-guia/src/App.tsx
@@ -19,17 +19,9 @@ function App() {
           animationDuration={2}
           pauseBetweenAnimations={1}
         />
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <br></br>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Sem enrolação. Filtramos o que realmente funciona e entregamos o método certo para evoluir no Clash.
       </p>
     </>
   )
